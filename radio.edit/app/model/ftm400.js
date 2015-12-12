25d60757a2d9eb6a6331899a71e010912013d089
jrohrssen <johnrohrssen@gmail.com>
2015-12-12 15:09:16 -0800

    Latest work in progress. Defining memory mapping

--- radio.edit/app/model/ftm400.js
+++ radio.edit/app/model/ftm400.js
@@ -19,8 +19,8 @@ var memoryBLabelStart = memoryALabelStart + memoryChannelLabels * labelSize;
 
 var labelCharSet = '0123456789' +
 				   'ABCDEFGHIJKLMNOPQRSTUVWXYZ'+
-				   'abcdefghijklmnopqrstuvwxyz' + 
-				   '!"#$%&`()*+,-./:;<=>?@[\\]^_`{|}~?????? ' + 
+				   'abcdefghijklmnopqrstuvwxyz' +
+				   '!"#$%&`()*+,-./:;<=>?@[\\]^_`{|}~?????? ' +
 				   '????????????????????????????????????????' +
 				   '????????????????????????????????????????' +
 				   '???????????';
@@ -36,131 +36,147 @@ var POWER_LEVELS = [ 'Hi', 'Mid', 'Low' ];
 var SKIPS = ['', 'S', 'P']
 
 
-var ftm400_map = 
+var ftm400_map =
 {
-	model:'FTM-400',
-	fileMarker: 'AH034$',
-	
-	
-	settings: 
-	{
-		callsign: 
-		{
-			start:696,
-			size:10,
-			encoding: 'ascii'
-		},
-		aprs:{}
+	vendor: 'Yaesu',
+	model: 'FTM-400',
+
+	file: {
+		marker: 'AH034$',
+		size: 65536
 	},
-	
+
+	serial: {
+		buad: 48000
+	},
+
+	settings:
+	{
+		callsign:
+		{
+			start: 0x2B8,
+			size: 10,
+			encoding: 'ascii',
+			fill: 255
+		},
+		aprs:{
+			callsign:
+			{
+				start: 0x508,
+				size: 8,
+				encoding: 'ascii',
+				fill: 202
+			},
+		}
+	},
+
 	bands:
 	[
 		{
 			name:'A',
-			channels: { start: 2048 },
-			labels: { start: 18624 }
+			channels: { type:'array', start: 2048, item:'channel', count: 500 },
+			labels: { type:'array', start: 18624, item:'label', count:518 }
 		},
 		{
 			name:'B',
-			channels: { start: 10336 },
-			labels: { start: 22768 }
+			channels: { type:'array', start: 10336, item:'channel', count: 500 },
+			labels: { type: 'array', start: 22768, item:'label', count: 518 }
 		}
 	],
-	
-	channel:
+
+s	channel:
 	{
-		count: 500,
 		size: 16,
 		fields:
 		{
 			used:
-			{ 
+			{
 				label: 'Used',
 				encoding: 'bool',
 				bits: [0, 1, false]
 			},
-			
+
 			skip:
-			{ 
+			{
 				label: 'Skip',
 				encoding: 'bool',
 				bits: [1, 2, false]
 			},
-			
+
 			mode:
-			{ 
+			{
 				label: 'Mode',
 				encoding: ['FM', 'AM', 'NFM', '', 'WFM'],
 				bits: [9, 3, false]
 			},
-			
+
 			oddsplit:
-			{ 
+			{
 				label: 'Odd Split',
 				encoding: 'bool',
 				bits: [13, 1, false]
 			},
-			
+
 			duplex:
-			{ 
+			{
 				label: 'Duplex',
 				encoding: ['', '', '-', '+', 'split'],
 				bits: [14, 2, false]
 			},
-			
+
 			tmode:
-			{ 
+			{
 				label: 'Transmit Mode',
 				encoding: ['', 'Tone', 'TSQL', '-RVT', 'DTCS', '-PR', '-PAG'],
 				bits: [41, 3, false]
 			},
-			
+
 			power:
-			{ 
+			{
 				label: 'Transmit Power',
 				encoding: [ 'Hi', 'Mid', 'Low' ],
 				bits: [72, 2, false]
 			},
-			
+
 			tone:
-			{ 
+			{
 				label: 'Transmit Tone',
 				encoding: [],
 				bits: [74, 2, false]
 			},
 
-			dtcs:				
-			{ 
+			dtcs:
+			{
 				label: 'DTCS',
 				encoding: [],
 				bits: [81, 7, false]
 			},
 
-			showalpha:				
-			{ 
+			showalpha:
+			{
 				label: 'Show Alpha',
 				encoding: 'bool',
 				bits: [88, 1, false]
 			},
 
-			offset:				
-			{ 
+			offset:
+			{
 				label: 'Offset',
 				encoding: [],
 				bits: [104, 8, false]
 			},
 
-			freq:				
-			{ 
+			freq:
+			{
 				label: 'Frequency',
 				units: 'Mhz',
 				encoding: 'bcd',
 				start: 2,
 				size: 3
 			},
-			
+
 			split:
-			{ 
+			{
 				label: 'Frequency Split',
 				units: 'Mhz',
 				encoding: 'bcd',
@@ -169,20 +185,18 @@ var ftm400_map =
 			}
 		}
 	},
-	
-	labels: {
-		count: 518,
+
+	label: {
 		size: 8,
-		
 		fields:
 		{
 			label:
-			{ 
+			{
 				label: 'Label',
 				encoding: '0123456789' +
-					'ABCDEFGHIJKLMNOPQRSTUVWXYZ'+
-					'abcdefghijklmnopqrstuvwxyz' + 
-					'!"#$%&`()*+,-./:;<=>?@[\\]^_`{|}~?????? ' + 
+					'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+					'abcdefghijklmnopqrstuvwxyz' +
+					'!"#$%&`()*+,-./:;<=>?@[\\]^_`{|}~?????? ' +
 					'????????????????????????????????????????' +
 					'????????????????????????????????????????' +
 					'???????????',
@@ -203,19 +217,19 @@ function ftm400Service($q) {
 	function loadRadio(buffer) {
 		//buffer = new Buffer(100);
 	    fs.writeFile( 'app/data/maps/ftm-400.json', JSON.stringify(ftm400_map, undefined, 2) );
-		
+
 		var radio = {
 			name: 'FTM-400',
 			error: '',
 			map: ftm400_map,
 			data: {},
 			settings: {},
-			bands: [{ 
+			bands: [{
 					name: 'A',
 					channels: [],
 				  	labels: [],
 					max: 500
-				}, { 
+				}, {
 					name: 'B',
 					channels: [],
 				  	labels: [],
@@ -237,50 +251,50 @@ function ftm400Service($q) {
 
 		readMemory(memoryAStart, radio, 0, buffer);
 		readMemory(memoryBStart, radio, 1, buffer);
-		
+
 		readLabel(memoryALabelStart, radio, 0, buffer);
 		readLabel(memoryBLabelStart, radio, 1, buffer);
 
 		return radio;
 	}
 
-	
+
 	function decodeFrequenceMhz( buffer )
 	{
 		return 0;
 	}
-	
+
 	function encodeFrequenceMhz( freq )
 	{
 		return bcd.encode(freq*100.0, 3);
 	}
-	
+
 	function setChannelData( channel, channelDataInfo, bitBuffer )
 	{
-		
+
 	}
-	
+
 	function readMemory(memStart, radio, subRadio, buffer) {
 		for (var i = 0; i < memoryChannels; ++i) {
 			var start = memStart + i * channelSize;
 			var end = start + channelSize;
-			
+
 			var bits = new bit.BitView( buffer.slice(start, end));
 			var used = bits.getBits( 0, 1, false);
-			
-			
+
+
 			if( used == 0 )
-				continue; 
-			
-			
+				continue;
+
+
 			var freqStart = start +2;
-			
+
 			var channel = {
 				id: i+1,
 				start: start,
 				end: end,
 				raw: buffer.slice(start, end),
-				
+
 				data: {
 					used: [used, used ? true : false],
 					skip: [bits.getBits( 1, 2, false), false],
@@ -298,13 +312,13 @@ function ftm400Service($q) {
 				}
 				  //decodeFrequenceMhz( rawFreq ),
 			};
-			
+
 			channel.data["used"] = []
-			
+
 			radio.bands[subRadio].channels[i] = channel;
 		}
 	}
-	
+
 	function decodeLabelString( rawLabel )
 	{
 		var label = "";
@@ -312,25 +326,25 @@ function ftm400Service($q) {
 			var char = rawLabel[index];
 			if(char == 202 )
 				break;
-			
+
 			label += labelCharSet[char];
 		}
-		
+
 		return label;
 	}
-	
+
 	function readLabel(memStart, radio, subRadio, buffer) {
 		for (var i = 0; i < memoryChannelLabels; ++i) {
 			var start = memStart + i * labelSize;
 			var end = start + labelSize;
-			
+
 			var label = decodeLabelString( buffer.slice(start, end) );
-			
+
 			radio.bands[subRadio].labels[i] = label;
 		}
 	}
 
 	function saveRadio(file, radio) {
-		
+
 	}
 }
